e8e6381fa4171161db77dba545c1698b06ff0ffc
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-07-05 19:31:05 +0800

    1
    1

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let tileSize = 100;
-
+let calDepth = 3;
+let maxDepth = 6;
 
 function setup() {
     createCanvas(800,800);
@@ -23,7 +24,36 @@ function mousePressed() {
 
 
 function initializeHtmlElements() {
-    let paraBody = document.createElement('p');
-    paraBody.innerText = 'Control Panel';
+    const titlePara = document.createElement('h3');
+    const buttonPlus = document.createElement('button');
+    const buttonMinus = document.createElement('button');
+    const displayPara = document.createElement('p')
+    const addDepth = _ => {
+         displayPara.innerText = `Thinking ${++calDepth} moves ahead`;
+         console.log(calDepth);
+         console.log('Add');
+    };
     
+    const minusDepth = _ =>{
+         displayPara.innerText = `Thinking ${--calDepth} moves ahead`;
+         console.log(calDepth);
+         console.log('Minus');
+    };
+    
+    buttonPlus.class = 'button plus';
+    buttonPlus.innerText = "+";
+    buttonMinus.class = 'button minus';
+    buttonMinus.innerText = "-";
+
+    titlePara.innerText = 'Control Panel';
+    displayPara.innerText = `Thinking ${calDepth} moves ahead`;
+    document.body.appendChild(titlePara);
+    document.body.appendChild(displayPara);
+    document.body.appendChild(buttonPlus);
+    document.body.appendChild(buttonMinus);
+    
+
+    buttonPlus.addEventListener('click', addDepth);
+    buttonMinus.addEventListener('click', minusDepth);
+
 }
